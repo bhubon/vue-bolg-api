d47975c3de6d7577f0ef4af2242d37ae0df76c3c
Bhubon Sutradhar Nil <sdbhubon@gmail.com>
2023-09-01 22:42:05 +0600

    Complete with error fixing

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,7 +5,7 @@ defineProps(['categories'])
 <template>
     <nav class="bg-white border-t-2 border-gray-200 border-b-2 mt-1">
         <div class="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" class="flex items-center">
+            <a href="/vue/blog" class="flex items-center">
                 <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
             </a>
