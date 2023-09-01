8ae8b04f2de04993a71c99b534fcc50fef5d8b4c
Bhubon Sutradhar Nil <sdbhubon@gmail.com>
2023-09-01 22:39:41 +0600

    Complete vue basic blog with api

--- src/components/BlogDetails.vue
+++ src/components/BlogDetails.vue
@@ -1,13 +1,23 @@
 <script setup>
-
+defineProps(['detailsNewas'])
 </script>
 
 <template>
     <div>
-        <h2>Blog details</h2>
+        <a href="/vue/blog" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4 inline-block">Back</a>
+        <article class="flex flex-col border-[1px] border-black-100 rounded-md">
+            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
+                <img class="object-cover w-full h-52 dark:bg-gray-500 custom-border h-auto" :src="detailsNewas.img">
+            </a>
+            <div class="flex flex-col flex-1 p-6">
+                <a rel="noopener noreferrer" href="#" :aria-label="detailsNewas.title"></a>
+                <a href="">
+                    <h3 class="flex-1 py-2 text-lg font-semibold leadi">{{ detailsNewas.title }}</h3>
+                </a>
+                <p>{{ detailsNewas.content }}</p>
+            </div>
+        </article>
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/BlogGrid.vue
+++ src/components/BlogGrid.vue
@@ -7,12 +7,15 @@ defineProps(['categories'])
 
 let latesNews = ref([]);
 
+let laoder = ref(true)
+
 async function getBlog() {
     let blog = axios.get('https://basic-blog.teamrabbil.com/api/post-newest')
     blog.then((response) => {
         if (response['data']) {
             latesNews.value = response['data'];
-            console.log(latesNews)
+            // console.log(latesNews)
+            laoder.value = false
         }
     })
 }
@@ -23,11 +26,17 @@ onMounted(() => {
 let detailsNewas = ref('');
 
 function getBlogDetails(blog_id) {
+    laoder.value = true
     let blog = axios.get('https://basic-blog.teamrabbil.com/api/post-details/' + blog_id)
     blog.then((response) => {
-        if (response['data']) {
-            detailsNewas.value = response['data']['postDetails'];
+        if (response['data']['postDetails']) {
+            detailsNewas.value = {};
+            detailsNewas.value = response['data']['postDetails'] ? response['data']['postDetails'] : '';
             console.log(detailsNewas.value)
+            laoder.value = false
+        }else{
+            alert("Api has no data for this blog id");
+            laoder.value = false
         }
     })
 }
@@ -38,19 +47,20 @@ function getBlogDetails(blog_id) {
 <template>
     <section class="py-6 sm:py-12">
         <div class="container p-6 mx-auto space-y-8">
-            <div class="space-y-2 text-center">
-                <h2 class="text-3xl font-bold">সাম্প্রতিক খবর</h2>
+            <div v-show="detailsNewas == ''">
+                <div class="space-y-2 text-center">
+                    <h2 class="text-3xl font-bold mb-4">সাম্প্রতিক খবর</h2>
+                </div>
+                <div class="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
+                    <BlogGridItem v-for="newsItem in latesNews" :key="newsItem.id" :newsItem="newsItem"
+                        :category="categories[newsItem.category_id] ? categories[newsItem.category_id] : ''"
+                        @click.prevent="getBlogDetails(newsItem.id)" />
+                </div>
             </div>
-            <div class="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
-                <BlogGridItem v-for="newsItem in latesNews" :key="newsItem.id" :newsItem="newsItem"
-                    :category="categories[newsItem.category_id] ? categories[newsItem.category_id] : ''"
-                    @click.prevent="getBlogDetails(newsItem.id)" />
-            </div>
 
-            <BlogDetails v-show="detailsNewas.length > 0" />
+            <BlogDetails v-show="detailsNewas != ''" :detailsNewas="detailsNewas" />
 
-
-            <div v-show="latesNews.length == 0" class="flex items-center justify-center ">
+            <div v-show="laoder" class="flex items-center justify-center ">
                 <div class="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin"></div>
             </div>
         </div>
